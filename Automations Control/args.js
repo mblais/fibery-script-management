77e4945fa210ea0075aca21c24f5f20ee9fc4bc7
Matt Blais <rasha808@gmail.com>
2023-10-19 16:05:05 -0700

    comments

--- Automations Control/args.js
+++ Automations Control/args.js
@@ -7,34 +7,41 @@ const { parseArgs }                 = require('node:util')              // https
 const { execFileSync }              = require('node:child_process')
 
 const {log,warn,error}              = console
+const stringify = (arg) => JSON.stringify(arg,null,2)
 
-// Parse command line options
 const commandLineOptions = {
-    domain: { type: 'string' },     // FIBERY_DOMAIN
-    // bar: { type: 'string',   short: 'b' },
-    // baz: { type: 'string',   short: 'z' },
+    domain: { type: 'string',       short: 'd' },     // FIBERY_DOMAIN
+    space:  { type: 'string',       short: 's' },
+    type:   { type: 'string',       short: 't' },
+    button: { type: 'string',       short: 'b' },
+    rule:   { type: 'string',       short: 'r' },
+    all:    { type: 'boolean',      short: 'a' },
 }
-// const args = [ '--domain', 'jrp.fibery.io', 'pos1', 'pos2' ]   // Test args for parseArgs
-const args = process.argv.slice(2)
+
+const args = [ '--domain', 'jrp.fibery.io', '-b.', '-r*', 'pos2' ]   // Test args for parseArgs
+// const args = process.argv.slice(2)
+
 const {values: options, positionals} = parseArgs({ args, options: commandLineOptions, allowPositionals: true })
-if( options.domain ) process.env.FIBERY_DOMAIN = options.domain
+log(`values:`, stringify(options))
+log(`postionals:`, stringify(positionals))
 
-// Validate inputs
-const FIBERY = process.env.FIBERY
-assert.ok( FIBERY, 'FIBERY env var should be the path to the root of the Fibery tree' )
-assert.doesNotMatch( FIBERY, /[;:<>|$`]/, `dangerous shell characaters in FIBERY env var: ${FIBERY}` )
+// if( options.domain ) process.env.FIBERY_DOMAIN = options.domain
+// // Validate inputs
+// const FIBERY = process.env.FIBERY
+// assert.ok( FIBERY, 'FIBERY env var should be the path to the root of the Fibery tree' )
+// assert.doesNotMatch( FIBERY, /[;:<>|$`]/, `dangerous shell characaters in FIBERY env var: ${FIBERY}` )
 
-// Call fiberyConfig.sh to get additional environment vars for the selected Fibery domain
-const cfg = join(FIBERY, 'fiberyConfig.sh')
-if (fs.statSync(cfg)) {
-    const moreEnvVars = execFileSync(cfg, ['-0', process.env.FIBERY_DOMAIN ?? '']).toString()
-    // Add the additional env vars to process.env
-    for( const line of moreEnvVars.split('\0') ) {
-        const [, name, value] = line.match( /(\w+)=([\S\s]*)/ ) ?? []
-        if (name) process.env[name] = value
-    }
-}
+// // Call fiberyConfig.sh to get additional environment vars for the selected Fibery domain
+// const cfg = join(FIBERY, 'fiberyConfig.sh')
+// if (fs.statSync(cfg)) {
+//     const moreEnvVars = execFileSync(cfg, ['-0', process.env.FIBERY_DOMAIN ?? '']).toString()
+//     // Add the additional env vars to process.env
+//     for( const line of moreEnvVars.split('\0') ) {
+//         const [, name, value] = line.match( /(\w+)=([\S\s]*)/ ) ?? []
+//         if (name) process.env[name] = value
+//     }
+// }
 
 // Dump Fibery env vars:
-log( Object.fromEntries( Object.keys(process.env).filter( k => k.match(/fibery/i) ).map( k => [k, process.env[k]] ) ) )
+// log( Object.fromEntries( Object.keys(process.env).filter( k => k.match(/fibery/i) ).map( k => [k, process.env[k]] ) ) )
  
