cd327b059fc1f6ec1fe469c204ac6f1627c5c51a
Matt Blais <rasha808@gmail.com>
2023-10-26 08:12:04 -0700

    Tweaks

--- fiberyConfig.sh
+++ fiberyConfig.sh
@@ -3,29 +3,29 @@
 # If the first arg is "-0" then output lines will be termianted with \0 instead of \n
 # Set the FIBERY_DOMAIN env var, or pass its value as an arg
 
-[[ $1 = '-0' ]] && { declare nulls=1; shift; }
-declare FIBERY_DOMAIN=${1:-$FIBERY_DOMAIN}
+declare format='%s=%s\n'
+[[ $1 = '-0' ]] && { shift; format='%s=%s\0'; }
+declare -x FIBERY_DOMAIN=${1:-$FIBERY_DOMAIN}
 set -e
 
-case ${FIBERY_DOMAIN:?} in
+case $FIBERY_DOMAIN in
     'jrp.fibery.io') 
         declare -x FIBERY_API_KEY='5ca15987.3c0091232ae3a06a509ca0601213f26a844'
-        declare -x FIBERY_SITE_SID='s%3AsegBmYeworlOYW_tL_E5MHL6A5yU2Ks5.ZwtVC8hInvoICYcWjM%2B%2FuEECRf05mF407RF%2BxccVWI4'
         ;;
     'jrp-zoho.fibery.io') 
         declare -x FIBERY_API_KEY='e3f705b1.5d343402dae27d46f53965264d009909ce6'
-        declare -x FIBERY_SITE_SID=
         ;;
     *)
-        echo "${BASH_SOURCE[*]} - unrecognized FIBERY_DOMAIN: $FIBERY_DOMAIN" >&2
-        exit 1
+        : "${Unknown_FIBERY_DOMAIN?\"$FIBERY_DOMAIN\"}"
 esac
 
-# Misc functions:  Escape a graphQL query (JSON) string for curl
+# Misc functions: Escape a graphQL query (JSON) string for curl
 escape()   { perl -e '$_=do{local $/; <STDIN>}; s/\t/\\t/g; s/"/\\"/g; s/\n/\\n/g; print'; }
 unescape() { sed 's/\\t/\t/g; s/\\"/"/g; s/\\r//g; s/\\n/\n/g; s/\\\\/\\/g'; }
 
-# Echo relevant env vars
-for n in FIBERY_DOMAIN FIBERY_API_KEY FIBERY_SITE_SID; do
-    [[ $nulls ]] && printf '%s=%s\0' "$n" "${!n}" || echo "$n=${!n}"
+# Echo FIBERY env vars
+
+for var in FIBERY_DOMAIN FIBERY_API_KEY; do
+    declare value=${!var}
+    printf "$format" "$var" "$value"
 done
