42af511f7d88a4575fdc7bb0666569753d4916d2
Matt Blais <281745+mblais@users.noreply.github.com>
2023-11-26 19:33:59 -0800

    Initial commit

---
+++ jrp.fibery.io/SPACE~ Workflows/DB~ WTask/BUTTON~ TEST Clone entity from Template ~b3ca.js
@@ -0,0 +1,3 @@
+//.fibery SCRIPTID=655e7b3f7f5fce9023b2b69f ACTIONID=098c3558-0298-4bbf-a786-2034e0b8b3ca
+
+//+include @/cloner.js
---
+++ jrp.fibery.io/SPACE~ Workflows/DB~ WTask/RULE~ Clone entity from Template ~9b5d.js
@@ -0,0 +1,3 @@
+//.fibery SCRIPTID=655e627c7f5fce9023b224da ACTIONID=c6baf4d3-f9ce-4a7c-8f5b-147557bc9b5d
+
+//+include @/cloner.js
